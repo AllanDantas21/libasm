35a41bbbcfc99651c0b755580ec0e11cfc1e8fcf
allandantas21 <allandantas21@gmail.com>
2026-07-05 13:00:22 -0300

    refactor: set errno in write and read

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,7 +1,18 @@
 section .text
 global ft_read
+extern __errno_location
 
 ft_read:
     mov rax, 0
     syscall
+    test rax, rax
+    js   .error
+    ret
+
+.error:
+    neg rax
+    push rax
+    call __errno_location wrt ..plt
+    pop qword [rax]
+    mov rax, -1
     ret
--- src/ft_write.s
+++ src/ft_write.s
@@ -1,7 +1,18 @@
 section .text
 global ft_write
+extern __errno_location
 
 ft_write:
     mov rax, 1
     syscall
+    test rax, rax
+    js   .error
+    ret
+
+.error:
+    neg rax
+    push rax
+    call __errno_location wrt ..plt
+    pop qword [rax]
+    mov rax, -1
     ret
